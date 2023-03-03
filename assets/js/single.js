2327d01a27b3832c5fecc0f2005e1e9a17f06ba3
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-03 10:00:14 -0800

    some minor changes

---
+++ assets/js/single.js
@@ -0,0 +1,12 @@
+let dino_1 = document.getElementById("dino_one");
+dino_1.addEventListener('click', choose())
+
+let choice = false;
+
+if (choice) {
+    choice = true;
+}
+else {
+    let dino_1 = document.getElementById("dino_two");
+}
+
