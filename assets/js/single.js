725c1cf5415874cf17278e812e47b1cad3885990
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-03 11:27:30 -0800

    FF

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,34 @@ let choice = false;
 
 if (choice) {
     choice = true;
+    let main = document.getElementById("dino_one")
 }
 else {
-    let dino_1 = document.getElementById("dino_two");
+    let main = document.getElementById("dino_two");
 }
 
+card = [
+    ['', 3],
+    ['', 4],
+    ['', 5],
+    ['', 6],
+    ['', 3],
+    ['', 3],
+    ['', 4],
+    ['', 4],
+    ['', 5],
+    ['', 5],
+    ['', 6],
+    ['', 6]
+]
+
+let cards = card.random;
+
+let card_amount = cards.length
+
+function start_turn() {
+    if (card_amount < 5) {
+        card++
+    }
+    for()
+}
