dedf14372faacc0393126a8ec1a08ab320e40b19
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-06 15:49:22 -0800

    d

--- assets/js/single.js
+++ assets/js/single.js
@@ -11,42 +11,42 @@ let board = [
 ]
 
 let dino_1 = document.getElementById("dino_one");
-dino_1.addEventListener('click', choice())
+// dino_1.addEventListener('click', choice())
+console.log(dino_1)
+// let choice = false;
 
-let choice = false;
+// if (choice) {
+//     choice = true;
+//     let main = document.getElementById("dino_one")
+// }
+// else {
+//     let main = document.getElementById("dino_two");
+// }
 
-if (choice) {
-    choice = true;
-    let main = document.getElementById("dino_one")
-}
-else {
-    let main = document.getElementById("dino_two");
-}
+// let card = [
+//     ['', 3],
+//     ['', 4],
+//     ['', 5],
+//     ['', 6],
+//     ['', 3],
+//     ['', 3],
+//     ['', 4],
+//     ['', 4],
+//     ['', 5],
+//     ['', 5],
+//     ['', 6],
+//     ['', 6]
+// ]
 
-let card = [
-    ['', 3],
-    ['', 4],
-    ['', 5],
-    ['', 6],
-    ['', 3],
-    ['', 3],
-    ['', 4],
-    ['', 4],
-    ['', 5],
-    ['', 5],
-    ['', 6],
-    ['', 6]
-]
+// let cards = card.random;
 
-let cards = card.random;
+// let card_amount = cards.length
 
-let card_amount = cards.length
-
-function start_turn() {
-    if (card_amount != 5) {
-        card++
-    }
-}
+// function start_turn() {
+//     if (card_amount != 5) {
+//         card++
+//     }
+// }
 
 
 // Only in single player
