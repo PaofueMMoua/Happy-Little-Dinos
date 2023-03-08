f11c1f74788fa2ba075ee95ac77f34d85f2b762b
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-08 14:54:22 -0800

    better deck

---
+++ assets/js/multi.js
@@ -0,0 +1,24 @@
+var values = ['1','2','3','4','5','6','Disaster-Insurance','Score-Sapper','Disaster-Redirect', 'Score-Booster'];
+var amount = ['_1','_2','_3','_4','_5','_6','_7'];
+var deck = new Array();
+
+function newDeck() {
+    deck = new Array();
+    for (var i = 0; i < values.length; i++) {
+        for (var x = 0; x < ammount.length; x++) {
+            var instant = parseInt(values[i]);
+            if ( values[i] == 'Score-Sapper') {
+                
+            }
+            if (values[i] == 'Disaster-Insurance') {
+                disaster_card = 0;
+            }
+            if (values[i] == 'Disaster-Redirect') {
+                disaster_card + opponent_disaster_area[];
+            }
+            if (values[i] == 'Score-Booster') {
+
+            }
+        }
+    }
+}
--- assets/js/single.js
+++ assets/js/single.js
@@ -15,7 +15,7 @@ let deckCount = 0;
 const deck = ((deckcount) => {
     deckCount = Math.min(5, Math.max(1, isNaN(deckCount) ? 2 : deckCount | 0));
     const value = '1,2,3,4,5,6'.split(",");
-    const amount = '-1,-2,-3,-4,-5,-6,-7'.split(',');
+    const amount = '_1,_2,_3,_4,_5,_6,_7'.split(',');
     const deck = [];
     const randIdx = () => Math.random() * deck.length | 0;
     return (Object.freeze({
@@ -46,6 +46,17 @@ console.log(deck.handA(deck.drawCards(5)));
 console.log(deck.handA(deck.drawCards(5)));
 console.log(deck.handA(deck.drawCards(5)));
 console.log(deck.handA(deck.drawCards(5)));
+
+let player_hand = deck.handA(deck.drawCards(5));
+// Draw Cards
+if(player_hand < 5 || player_hand != deck.handA) {
+    deck.drawCards(1) + deck.handA;
+}
+// playing a point card
+if(player_hand) {
+    player_card 
+}
+
 // drawing cards
 // function drawCard() {
 //     var count = document.getElementById("")
