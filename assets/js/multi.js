61278deb22dd92810100f906d13b92e9ecd7ea35
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-08 15:25:00 -0800

    Update multi.js

--- assets/js/multi.js
+++ assets/js/multi.js
@@ -1,24 +1,53 @@
 var values = ['1','2','3','4','5','6','Disaster-Insurance','Score-Sapper','Disaster-Redirect', 'Score-Booster'];
 var amount = ['_1','_2','_3','_4','_5','_6','_7'];
 var deck = new Array();
-
+// deck creation
 function newDeck() {
     deck = new Array();
     for (var i = 0; i < values.length; i++) {
         for (var x = 0; x < ammount.length; x++) {
             var instant = parseInt(values[i]);
             if ( values[i] == 'Score-Sapper') {
-                
+
             }
             if (values[i] == 'Disaster-Insurance') {
                 disaster_card = 0;
             }
             if (values[i] == 'Disaster-Redirect') {
-                disaster_card + opponent_disaster_area[];
+                disaster_card + opponent_disaster_area;
             }
             if (values[i] == 'Score-Booster') {
-
+                point_area +=2;
             }
+            var card = {value:values[i], amounts:amount[x], instants:instant};
+            deck.push(card)
         }
     }
 }
+// making random
+function random() {
+    for(var i = 0; i < 100; i++)
+    {
+        var location1 = Math.floor((Math.random() * deck.length));
+        var location2 = Math.floor((Math.random()) * deck.length);
+        var temp = deck[location1];
+        deck[location1] = deck[location2];
+        deck[location2] = temp;
+    }
+}
+// Creating the player
+var players = new Array();
+function newplayers(num){
+    players = new Array();
+    for(vari=0;i<=num;i++) {
+        let hand = new Array();
+        let player = {Name: 'User' + i, ID:i, Score: 0, Hand : hand};
+        players.push(player);
+    }
+}
+
+function PlayerUi() {
+    document.getElementById('user').innerHTML = '';
+    
+}
+// https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
