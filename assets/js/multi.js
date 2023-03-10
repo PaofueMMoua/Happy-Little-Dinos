620f7023bc1418adfa62ed8e88ee4c1e2c35b112
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-09 19:50:54 -0800

    Comments
    Adding comments to make the JS more easily accessible and readable

--- assets/js/multi.js
+++ assets/js/multi.js
@@ -21,16 +21,21 @@ ND.addEventListener('click', random(newDeck))
 
 
 
-
-var values = ['1','2','3','4','5','6','Disaster-Insurance','Score-Sapper','Disaster-Redirect', 'Score-Booster'];
-var amount = ['_1','_2','_3','_4','_5','_6','_7'];
-var deck = new Array();
+//cards within the deck
+const values = ['1','2','3','4','5','6','Disaster-Insurance','Score-Sapper','Disaster-Redirect', 'Score-Booster'];
+// The ammount of each card
+const amount = ['_1', '_2', '_3', '_4', '_5', '_6', '_7'];
+// Making the deck an array for the cards to live in.
+let deck = new Array();
 // deck creation
 function newDeck() {
     deck = new Array();
-    for (var i = 0; i < values.length; i++) {
-        for (var x = 0; x < ammount.length; x++) {
-            var instant = parseInt(values[i]);
+    // Initializing the variable i to go by the amount of the values
+    for (let i = 0; i < values.length; i++) {
+        // initializing x to be the length of the ammount of cards for easier calculations
+        for (let x = 0; x < ammount.length; x++) {
+            let instant = parseInt(values[i]);
+            // Initializing the instant cards to do different things.
             if ( values[i] == 'Score-Sapper') {
             }
             if (values[i] == 'Disaster-Insurance') {
@@ -42,24 +47,26 @@ function newDeck() {
             if (values[i] == 'Score-Booster') {
                 point_area +=2;
             }
-            var card = {value:values[i], amounts:amount[x], instants:instant};
+            // The calculations for the cards
+            let card = {value:values[i], amounts:amount[x], instants:instant};
             deck.push(card)
         }
     }
 }
 // making random
 function random() {
-    for(var i = 0; i < 100; i++)
+    for(let i = 0; i < 100; i++)
     {
-        var location1 = Math.floor((Math.random() * deck.length));
-        var location2 = Math.floor((Math.random()) * deck.length);
-        var temp = deck[location1];
+        // Initializing the cards being randomized
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random()) * deck.length);
+        let temp = deck[location1];
         deck[location1] = deck[location2];
         deck[location2] = temp;
     }
 }
 // Creating the player
-var players = new Array();
+let players = new Array();
 
 function newplayers(num){
     players = new Array();
