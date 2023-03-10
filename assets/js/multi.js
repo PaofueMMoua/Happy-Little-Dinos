dab33427d39e5cc8ccb353d4d31ed2fbdc64233b
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-03-09 19:46:17 -0800

    adding a few things
    Helping with some doccumentations and some eventlisteners

--- assets/js/multi.js
+++ assets/js/multi.js
@@ -3,11 +3,24 @@
 import {joinRoom, selfId } from 'https://cdn.skypack.dev/trystero/ipfs';
 const user = {appId: 'HappyLittleDinosaurDemo'};
 
+const config = {appId: 'san_narciso_3d'}
+
+joinRoom({appId: 'Anonymouse', password:'dino-test'}, 'test')
+
 let myName = `Anonymouse`;
 
 function RandomId() {
     return Math.random().toString(2).substring(1,2);
 }
+
+// Used for connecting but will need to run first.
+//https://www.skypack.dev/view/trystero#encryption
+
+let ND = document.getElementById('') //Add in the input for getting the card.
+ND.addEventListener('click', random(newDeck))
+
+
+
 
 var values = ['1','2','3','4','5','6','Disaster-Insurance','Score-Sapper','Disaster-Redirect', 'Score-Booster'];
 var amount = ['_1','_2','_3','_4','_5','_6','_7'];
@@ -19,7 +32,6 @@ function newDeck() {
         for (var x = 0; x < ammount.length; x++) {
             var instant = parseInt(values[i]);
             if ( values[i] == 'Score-Sapper') {
-
             }
             if (values[i] == 'Disaster-Insurance') {
                 disaster_card = 0;
@@ -48,6 +60,7 @@ function random() {
 }
 // Creating the player
 var players = new Array();
+
 function newplayers(num){
     players = new Array();
     for(vari=0;i<=num;i++) {
@@ -59,6 +72,5 @@ function newplayers(num){
 
 // function PlayerUi() {
 //     document.getElementById('user').innerHTML = '';
-    
 // }
 // https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
